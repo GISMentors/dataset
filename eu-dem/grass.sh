8cb27b35dcde3d71100e738673445474281b1f6f
Martin Landa <landa.martin@gmail.com>
2015-06-16 20:10:04 +0200

    eu-dem: kompletni skript

--- eu-dem/grass.sh
+++ eu-dem/grass.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-### grass70 /work/geodata/grassdata/gismentors/user1/
-g.region rast=dmt res=100 -a
-r.resamp.interp in=dmt out=dmt100
-r.out.gdal in=dmt100 out=/tmp/dmt.tif type=UInt16 --o -f
+export GRASS_BATCH_JOB=./grass-batch.sh
+grass70 /work/geodata/grassdata/gismentors/user1/
 
+exit 0
--- eu-dem/import.sh
+++ eu-dem/import.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
+DB=gismentors
+SCHEMA=rastry
+
 ./grass.sh
-raster2pgsql -s 5514 -Y -C -t 400x400 /tmp/dmt.tif rastry.dmt | psql gismentors
+psql $DB -c "drop schema $SCHEMA cascade ; create schema $SCHEMA" 
+raster2pgsql -s 5514 -Y -t 400x400 /tmp/dmt.tif $SCHEMA.dmt | psql $DB
 
 exit 0
