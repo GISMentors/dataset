3aa008e9900e635354dc33a45fdfea80d112c5fb
Martin Landa <landa.martin@gmail.com>
2016-03-10 12:00:35 +0100

    fix epsg 5514 (gis.lab)

--- postgis/epsg-5514.sh
+++ postgis/epsg-5514.sh
@@ -4,8 +4,8 @@
 wget http://epsg.io/5514-1623.sql > /dev/null
 
 #nahraj 5514 do db
-psql gismentors -c "delete from spatial_ref_sys where srid = 5514"
-psql gismentors -f 5514-1623.sql
+sudo psql -U postgres gismentors -c "delete from spatial_ref_sys where srid = 5514"
+sudo psql -U postgres gismentors -f 5514-1623.sql
 rm 5514-1623.sql
 
 exit 0
