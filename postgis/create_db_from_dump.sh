da8b2164f02dd03bd462d9132aac6b84803e24de
Martin Landa <landa.martin@gmail.com>
2016-03-15 11:15:01 +0100

    create from dump : final clean up

--- postgis/create_db_from_dump.sh
+++ postgis/create_db_from_dump.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 
+DB=gismentors
+
 # vytvorit DB
-createdb gismentors;
+createdb $DB 2>/dev/null || (dropdb $DB && createdb $DB)
 
 # stahnout dump
 wget http://training.gismentors.eu/geodata/postgis/gismentors.dump
 
 # nahrat dump do DB
-pg_restore gismentors.dump | psql gismentors
+pg_restore gismentors.dump | psql $DB
 
 # stahnout definici S-JTSK s transformaci CUZK pro CR, chyba max 1m
-./epsg-5514.sh
+./epsg-5514.sh $DB
 
 # smazat dump
 rm -f gismentors.dump
--- postgis/epsg-5514.sh
+++ postgis/epsg-5514.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 
+if test -z "$1" ; then
+    DB="gismentors"
+else
+    DB=$1
+fi
+
 #stahni definici S-JTSK s transformaci CUZK pro CR, chyba max 1m
 wget http://epsg.io/5514-1623.sql > /dev/null
 
 #nahraj 5514 do db
-sudo psql -U postgres gismentors -c "delete from spatial_ref_sys where srid = 5514"
-sudo psql -U postgres gismentors -f 5514-1623.sql
+psql $DB -c "delete from spatial_ref_sys where srid = 5514"
+psql $DB -f 5514-1623.sql
 rm 5514-1623.sql
 
 exit 0
