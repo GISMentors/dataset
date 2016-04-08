4157b7639058ebc111b5ca93044f762c7d905442
Martin Landa <landa.martin@gmail.com>
2016-04-09 00:39:33 +0200

    fix ruian import script

--- ruian/import.sh
+++ ruian/import.sh
@@ -2,14 +2,17 @@
 
 export DB=gismentors
 
-git clone https://github.com/ctu-osgeorel/gdal-vfr.git
-cd gdal-vfr
+if [ ! -d gdal-vfr ] ; then
+    git clone https://github.com/ctu-osgeorel/gdal-vfr.git
+else
+    git pull
+fi
 
 ### import dat do schematu ruian
-./vfr2pg --type ST_UKSG --dbname $DB --schema ruian --geom GeneralizovaneHranice
+./gdal-vfr/vfr2pg --type ST_UKSG --dbname $DB --schema ruian --geom GeneralizovaneHranice
 
 ### import dat do schematu ruian_praha
-./vfr2pg --type OB_554782_UKSH --dbname $DB --schema praha_praha --geom OriginalniHranice
+./gdal-vfr/vfr2pg --type OB_554782_UKSH --dbname $DB --schema ruian_praha --geom OriginalniHranice
 
 ### prejmenovani atributu
 # cat vfr.txt | cut -d '|' -f2,3,4 | sed 's/ \+//g' |  awk -F'|' '{printf "alter table "$1"."$2" rename "$3" to geom;\n"}'
