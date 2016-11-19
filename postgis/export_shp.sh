6f6a1266d3db822985a9178d2f34f08799dff22b
Martin Landa <landa.martin@gmail.com>
2016-11-19 16:28:08 +0100

    postgis -> shp: generate also qpj file

--- postgis/export_shp.sh
+++ postgis/export_shp.sh
@@ -2,7 +2,10 @@
 
 DB=gismentors
 
-if [ -z "$1" ]; then
+SCRIPT=`realpath $0`
+SCRIPT_PATH=`dirname $SCRIPT`
+
+if [ -n "$1" ]; then
     DIR="$1"
 else
     DIR=/tmp/gismentors_shp
@@ -11,6 +14,7 @@ fi
 layers=`sudo psql -U postgres $DB -tA -F'.' -c"select f_table_schema,f_table_name from geometry_columns"`
 
 rm -rf $DIR && mkdir $DIR
+
 for layer in $layers; do
     schema=`echo $layer | cut -d'.' -f1`
     table=`echo $layer | cut -d'.' -f2`
@@ -20,7 +24,8 @@ for layer in $layers; do
         cd $schema
     fi
     echo "Exporting $schema.$table..."
-    sudo ogr2ogr -f 'ESRI Shapefile' -lco 'ENCODING=UTF-8' ${table}.shp "PG:dbname=$DB user=postgres" ${schema}.$table
+    sudo ogr2ogr -f 'ESRI Shapefile' -lco 'ENCODING=UTF-8,RESIZE=YES' ${table}.shp "PG:dbname=$DB user=postgres" ${schema}.$table
+    sudo cp $SCRIPT_PATH/epsg-5514.qpj ${table}.qpj
 done
 
 exit 0
