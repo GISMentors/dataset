722995a7e14b13f580b4f55c2c0b8ac8b18e1168
Martin Landa <landa.martin@gmail.com>
2018-12-11 20:20:33 +0100

    add gpkg export script

---
+++ postgis/export_gpkg.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+DB=gismentors
+
+SCRIPT=`realpath $0`
+SCRIPT_PATH=`dirname $SCRIPT`
+
+if [ -n "$1" ]; then
+    DIR="$1"
+else
+    DIR=/tmp/gismentors_gpkg
+fi
+
+layers=`sudo psql -U postgres psql $DB -tA -F'.' -c"select f_table_schema,f_table_name from geometry_columns"`
+
+rm -rf $DIR && mkdir $DIR
+
+for layer in $layers; do
+    schema=`echo $layer | cut -d'.' -f1`
+    table=`echo $layer | cut -d'.' -f2`
+    echo "Exporting $schema.$table..."
+    sudo ogr2ogr -append -f 'GPKG' $DIR/${schema}.gpkg -nln $table "PG:dbname=$DB" ${schema}.$table
+done
+
+exit 0
