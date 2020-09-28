e5fd5c47f2dac81485701bf6960cea54595d4bc2
Martin Landa <landa.martin@gmail.com>
2020-09-28 12:55:35 +0200

    osm default style added

--- osm/import.sh
+++ osm/import.sh
@@ -7,7 +7,7 @@ DB=gismentors
 SCRIPT=`realpath $0`
 SCRIPT_PATH=`dirname $SCRIPT`
 
-osm2pgsql -d $DB -p osm -E 3857 -C 8000 /tmp/czech-republic-latest.osm.bz2
+osm2pgsql -d $DB -p osm -E 3857 -C 8000 -S ${SCRIPT_PATH}/default.style /tmp/czech-republic-latest.osm.bz2
 
 psql $DB -f ${SCRIPT_PATH}/pozarni_stanice.sql
 psql $DB -f ${SCRIPT_PATH}/silnice.sql
