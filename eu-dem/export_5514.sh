37f6d35ffa35345a4228ec65c029af2b91e4028a
Martin Landa <landa.martin@gmail.com>
2021-03-20 23:55:26 +0100

    eu-dem pipeline cosmetics

--- eu-dem/export_5514.sh
+++ eu-dem/export_5514.sh
@@ -1,15 +1,16 @@
 #!/bin/sh 
 
 v.import input=/mnt/repository/gismentors/gpkg/ruian.gpkg layer=staty output=cr
+g.region vector=cr
 
 # dtm 25m
-g.region vector=cr
 r.proj location=grassloc-3035 mapset=PERMANENT input=dtm method=bilinear resolution=25
 r.colors map=dtm color=elevation
 # r.out.gdal input=dtm output=/mnt/repository/eu-dem/dmt.tif type=UInt16 -f
 
 # dmt 100m
 g.region res=100 -a
+r.mask vector=cr
 r.resamp.interp input=dtm output=dtm100 method=bilinea
 r.colors map=dtm100 color=elevation
 r.out.gdal input=dtm100 output=/mnt/repository/eu-dem/dmt100.tif type=UInt16 -f
